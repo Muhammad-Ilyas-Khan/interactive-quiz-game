c1c5abd9bfe525d81a3f79624711151694c4de2c
Muhammad-Ilyas-Khan <ilyask007@gmail.com>
2024-09-06 08:45:22 +0500

    create basic variables

---
+++ script.js
@@ -0,0 +1,21 @@
+var currentQuestionIndex = 0;
+var score = 0;
+const questions = [
+    {
+        question: "What is the capital of France?",
+        choices: ["Berlin", "Madrid", "Paris", "Rome"],
+        correctAnswer: "Paris"
+    },
+
+    {
+        question: "What is the capital of Spain?",
+        choices: ["Ankara", "Madrid", "Berlin", "Paris"],
+        correctAnswer: "Paris"
+    },
+
+    {
+        question: "What is the capital of Germany?",
+        choices: ["Beijing", "Buenos Aires", "Kyiv", "Berlin"],
+        correctAnswer: "Paris"
+    }
+]
