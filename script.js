1594f23f87ad23a4d36eb2ede745399969b827c4
Muhammad-Ilyas-Khan <ilyask007@gmail.com>
2024-09-06 08:56:51 +0500

    select elements for the HTML by their IDs or classes

--- script.js
+++ script.js
@@ -19,3 +19,9 @@ const questions = [
         correctAnswer: "Paris"
     }
 ]
+
+//selectiong elements from the HTLM by their IDs or classes
+var questionArea = document.getElementById('question-area'); //display question
+var answerButtons = document.quesrySelectorAll('.answer-btn'); // answer buttons
+var scoreCounter = document.getElementById('score-counter'); // score display element
+var nextQuestionButton = document.getElementById('next-question'); // next question button
